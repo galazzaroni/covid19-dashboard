b487425162154b3cc027a49b95fb39e1a9f901c1
Gonzalo <galazzaroni@protonmail.com>
2020-10-27 02:55:39 -0300

    chart OK

---
+++ src/app/interface/country.ts
@@ -0,0 +1,14 @@
+export interface Country {
+    Country: String,
+    CountryCode: String,
+    Province: String,
+    City: String,
+    CityCode: String,
+    Lat: String,
+    Lon: String,
+    Confirmed: Number,
+    Deaths: Number,
+    Recovered: Number,
+    Active: Number,
+    Date: Date
+}
---
+++ src/app/interface/summary.ts
@@ -0,0 +1,13 @@
+export interface Summary {
+    Country: String,
+    CountryCode: String,
+    Slug: String,
+    NewConfirmed: Number,
+    TotalConfirmed: Number,
+    NewDeaths: Number,
+    TotalDeaths: Number,
+    NewRecovered: Number,
+    TotalRecovered: Number,
+    Date: Date,
+    Premium: {}
+}
